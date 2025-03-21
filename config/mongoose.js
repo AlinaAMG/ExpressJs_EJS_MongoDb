75e6248b677c169994bc5d64057665e1fd5bd906
AlinaAMG <alina_gabur@yahoo.com>
2025-03-21 18:48:00 +0100

    Add files via upload
    Crash course expressJs,EJS and mongoDB

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://alina:alina82@cluster-test.ibpnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-test")
-    .then(() => {
-        console.log("DB is connected")
+mongoose.connect("mongodb+srv://alina:alina82@cluster-test.ibpnh.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster-test")
+    .then(result => {
+     console.log("DB is connected");
     })
-    .catch(err => console.log(err));
+.catch(err=>console.log(err))
