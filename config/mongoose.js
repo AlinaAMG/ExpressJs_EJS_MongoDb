f7914649b717143732d1a111abf47cd2418340ca
Alina <alina_gabur@yahoo.com>
2025-03-23 18:10:09 +0100

    files uploaded

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://alina:alina82@cluster-test.ibpnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-test")
-    .then(() => {
-        console.log("DB is connected")
-    })
-    .catch(err => console.log(err));
+mongoose
+  .connect(
+    'mongodb+srv://alina:alina82@cluster-test.ibpnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-test'
+  )
+  .then(() => {
+    console.log('DB is connected');
+  })
+  .catch((err) => console.log(err));
--- config/routes.js
+++ config/routes.js
@@ -1,21 +1,27 @@
-const express = require("express");
+const express = require('express');
 const route = express.Router();
-const userController = require("../controllers/userController")
+const userController = require('../controllers/userController');
 
 // routes
 
-route.get("/", userController.homePage);
+route.get('/', userController.homePage);
 
-route.post("/add-new-message", userController.addNewMessage);
+route.post('/add-new-message', userController.addNewMessage);
+
+route.get('/update/message/:id', userController.editMessagePage);
+
+route.post('/edit-message-form/:id', userController.editMessageForm);
+
 // For postman delete method
-route.delete("/delete/message/:id", userController.deleteMessage)
+route.delete('/delete/message/:id', userController.deleteMessage);
+
 // for the browser  button
-route.get("/delete/message/:id", userController.deleteMessage);
+// delete message
+route.get('/delete/message/:id', userController.deleteMessage);
 
-route.get("/update/message/:id", userController.editMessagePage);
+// Add comments
+route.post('/add/new-comments/:id', userController.addComments);
 
-route.post("/edit-message-form/:id", userController.editMessageForm);
-
-route.get("*", userController.notFoundPage);
+route.get('*', userController.notFoundPage);
 
 module.exports = route;
